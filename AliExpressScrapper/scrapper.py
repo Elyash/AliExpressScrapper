cebb023622e665d1334b25c747feb605ee8db4cb
Elyash <elyashiv12@gmail.com>
2024-09-26 20:41:46 +0300

    Added mongoDB service; Added functionality to scrapper to store the scrapped data in mongoDB

--- AliExpressScrapper/scrapper.py
+++ AliExpressScrapper/scrapper.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python
 """Main AliExpress scrapper module."""
 
 import time
+
+from pymongo import MongoClient
 
 from selenium import webdriver
 from selenium.common import exceptions
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -78,8 +82,33 @@ class AliExpressScrapper:
 
         return product_details
 
+
+class MongoDBInterface:
+    """An interface to connect with mongoDB."""
+
+    def __init__(self):
+        self.__client__ = MongoClient(
+            "mongodb", port=27017, username='admin', password='password'
+        )
+
+    def __del__(self):
+        self.__client__.close()
+
+    def store_gift(self, gift_details):
+        """Store a gift in the Gifts DB."""
+
+        self.__client__['MyFullstackProject']['Gifts'].insert_one(gift_details) # TODO: check the reutrn value 
+
+
 # Usage axample
 if __name__ == '__main__':
     example_url = 'https://he.aliexpress.com/item/1005005956410553.html?spm=a2g0o.best.moretolove.2.225c216f6uZIzX&_gl=1*sq3u1s*_gcl_aw*R0NMLjE3MjEyMzcyMjMuQ2p3S0NBancxOTIwQmhBM0Vpd0FKVDNsU2JTeVVqV0dKc05YQzZEWE5GbGZlQ3pUTUZBVDhBZHl1YUJJNXR1bzVuTXV2V3lTUlo4cGNob0N6MGdRQXZEX0J3RQ..*_gcl_dc*R0NMLjE3MjEyMzcyMjMuQ2p3S0NBancxOTIwQmhBM0Vpd0FKVDNsU2JTeVVqV0dKc05YQzZEWE5GbGZlQ3pUTUZBVDhBZHl1YUJJNXR1bzVuTXV2V3lTUlo4cGNob0N6MGdRQXZEX0J3RQ..*_gcl_au*MTk1NTk3NDA2My4xNzI3MDk5Nzcz*_ga*NzI1NjAxMTM1LjE2ODA4MDkyNDg.*_ga_VED1YSGNC7*MTcyNzE4MTU0MC4yMS4xLjE3MjcxODE1NzEuMjkuMC4w&gatewayAdapt=glo2isr'
+
     scrapper = AliExpressScrapper()
-    print(scrapper.scrape_product(example_url))
+    gifts_dbi = MongoDBInterface()
+
+    gift = scrapper.scrape_product(example_url)
+    gifts_dbi.store_gift(gift)
+    print(gift)
+
+    time.sleep(100000)
